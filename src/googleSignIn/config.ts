877e187292b85a6c72e4ee1731de603c1fbcf607
Jeya chandran s <jeyachandranjcs@gmail.com>
2025-06-16 16:22:45 +0530

    Update config.ts
    saving after giving key

--- src/googleSignIn/config.ts
+++ src/googleSignIn/config.ts
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkd6LWLqjvRyii55D1tfu9-jGq-mXr5E0",
-  authDomain: "my-personal-project-fb78d.firebaseapp.com",
-  projectId: "my-personal-project-fb78d",
-  storageBucket: "my-personal-project-fb78d.firebasestorage.app",
-  messagingSenderId: "796004564104",
-  appId: "1:796004564104:web:e130d2395fd3f2e05ef33d",
-  measurementId: "G-469TX2DV7G"
+  
 };
 
 // Initialize Firebase
