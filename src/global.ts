71c28fa7766320f88eb221e9bdff197da8555e0a
Jeya chandran s <jeyachandranjcs@gmail.com>
2025-06-25 15:01:38 +0530

    Update global.ts
    api changed

--- src/global.ts
+++ src/global.ts
@@ -1,1 +1,2 @@
-export const API="https://6597fc24668d248edf23dc91.mockapi.io/management-system"
+// export const API="https://6597fc24668d248edf23dc91.mockapi.io/management-system"
+export const API="https://684bef05ed2578be881d0a1d.mockapi.io/Foodlist/Work-stack"
